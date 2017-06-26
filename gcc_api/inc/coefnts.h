96a014bb1d65ea67b8a7b70ffffaf1f3f9c18ce7
lomassubedi <subedilomas@gmail.com>
2017-06-26 23:51:44 +0545

    Just created files.

---
+++ gcc_api/inc/coefnts.h
@@ -0,0 +1,13 @@
+/*
+ * coefnts.h
+ *
+ *  Created on: Jun 26, 2017
+ *      Author: LOMAS
+ */
+
+#ifndef COEFNTS_H_
+#define COEFNTS_H_
+
+
+
+#endif /* COEFNTS_H_ */
---
+++ gcc_api/inc/controller.h
@@ -0,0 +1,13 @@
+/*
+ * controller.h
+ *
+ *  Created on: Jun 26, 2017
+ *      Author: LOMAS
+ */
+
+#ifndef CONTROLLER_H_
+#define CONTROLLER_H_
+
+
+
+#endif /* CONTROLLER_H_ */
